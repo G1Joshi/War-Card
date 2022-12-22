039db468c603b4e9b5cbc76ca3946cf0024fbe7f
Jeevan Joshi <joshijeevan98@gmail.com>
2022-12-22 22:39:20 +0530

    initial commit

---
+++ War Card/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  War Card
+//
+//  Created by Jeevan Chandra Joshi on 22/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ War Card/War_CardApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_CardApp.swift
+//  War Card
+//
+//  Created by Jeevan Chandra Joshi on 22/12/22.
+//
+
+import SwiftUI
+
+@main
+struct War_CardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
